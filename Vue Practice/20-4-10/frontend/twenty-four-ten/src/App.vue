ad8128a13300fb14ee9790d1b936451330594f22
Alex Jackman <jackman.alex.w@gmail.com>
2021-08-03 16:33:45 -0600

    started from a clean slate, again :P

---
+++ Vue Practice/20-4-10/frontend/twenty-four-ten/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="20-4-10 Rule"/>
+  <p>[description for 20-4-10 rule of thumb for car financing]</p>
+</template>
+
+<script>
+import HelloWorld from './components/CalculatorInterface.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
