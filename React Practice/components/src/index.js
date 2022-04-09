16d7e925c3e4c5f5c6ddb6b0c8274a7a085a3c1d
JackmanAlex228 <jackman.alex.w@gmail.com>
2022-04-09 16:43:48 -0600

    Some changes for react practice

--- React Practice/components/src/index.js
+++ React Practice/components/src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className='ui container comments'>
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 16:45"
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 02:00" 
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>    
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 17:00" 
+                    content="I like the writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
