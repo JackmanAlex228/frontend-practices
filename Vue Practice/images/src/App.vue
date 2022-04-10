585c8587f9003a207536531c51ef67f88010dfc1
JackmanAlex228 <jackman.alex.w@gmail.com>
2022-04-10 15:55:25 -0600

    re-added images

---
+++ Vue Practice/images/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<script>
+
+// This is where the main app lives
+import AppHeader from './components/AppHeader';
+export default {
+    name: 'App',
+    components: {
+        AppHeader
+    }
+};
+</script>
