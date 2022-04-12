ec0ac02e23ed715e40558cfdcd9f6f6dd9ea5461
JackmanAlex228 <jackman.alex.w@gmail.com>
2022-04-12 14:55:06 -0600

    finished auth vuex modules

--- Vue Practice/images/src/store/modules/auth.js
+++ Vue Practice/images/src/store/modules/auth.js
@@ -15,3 +15,23 @@
  * Client ID: f82505ee0d28697
  * Client secret: 60818aa7079fad7583b6372d8c06f419e0ac03f1
  */
+
+const state = {
+    token: null
+};
+
+const getters = {
+    isLoggedIn: state => !!state.token
+};
+
+const actions = {
+    logout: ({ commit }) => {
+        commit('setToken', null);
+    }
+};
+
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
